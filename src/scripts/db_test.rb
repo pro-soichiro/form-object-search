53ed84f08bd5f949873df376e6a5279f68f1901e
pro-soichiro <dr.1987626@gmail.com>
2022-07-20 22:39:08 +0900

    scriptファイルを追加

---
+++ src/scripts/db_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+print 'DBのクエリ発行に関する負荷テストを実行しますか？(yes) > '
+
+unless gets.strip == 'yes'
+  puts 'キャンセルしました'
+  return
+end
+
+Prefecture.transaction do
+  convert_count = 0
+
+  10.times do |n|
+    Prefecture.create(name: "#{n + 1}回目")
+    convert_count = n
+    puts n + 1
+  end
+
+  puts "作成数: #{convert_count + 1}件"
+
+
+  print 'ロールバックしてよろしいでしょうか?(yes) > '
+
+  if gets.strip == 'yes'
+    raise ActiveRecord::Rollback
+    puts 'ロールバックしました。'
+  else
+    puts 'データを保存しました。'
+  end
+end
